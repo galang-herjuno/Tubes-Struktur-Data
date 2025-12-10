183a06682672e4f59044f43f651021f5a46a1b89
unknown <azizirhzzz@gmail.com>
2025-12-10 22:27:53 +0700

    Update penambahan fungsi delete dan addRelation

--- Univ.cpp
+++ Univ.cpp
@@ -17,9 +17,46 @@ Faculty* createFac(const string& nama) {
 }
 
 bool addRelation(University* univ, Faculty* fac) {
+    if (!univ || !fac) return false;
+
+    Relation* r = new Relation;
+    r->univ = univ;
+    r->fac = fac;
+    r->nextUniv = univ->head;
+    r->nextFac = fac->head;
+
+    univ->head = r;
+    fac->head = r;
+    return true;
 
 }
 
 
-void deleteUniv(University* head, const string& nama);
-void deleteFac(Faculty* head, const string& nama);
+void deleteUniv(University* head, const string& nama){
+    University* prev = nullptr;
+    University* curr = head;
+    while (curr) {
+        if (curr->nama == nama) {
+            if (prev) prev->next = curr->next;
+            delete curr;
+            return;
+        }
+        prev = curr;
+        curr = curr->next;
+    }
+}
+
+void deleteFac(Faculty* head, const string& nama){
+    Faculty* prev = nullptr;
+    Faculty* curr = head;
+    while (curr) {
+        if (curr->nama == nama) {
+            if (prev) prev->next = curr->next;
+            delete curr;
+            return;
+        }
+        prev = curr;
+        curr = curr->next;
+    }
+
+}
