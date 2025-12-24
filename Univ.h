365f2de4b03a2d2d5c59d6fb73676a4a275a631e
galang-herjuno <142505672+galang-herjuno@users.noreply.github.com>
2025-12-24 13:40:21 +0700

    Update Univ.h

--- Univ.h
+++ Univ.h
@@ -36,22 +36,27 @@ void insertFirstUniv(University*& head, const string& nama, int tahun, int jmlMh
 void insertLastUniv(University*& head, const string& nama, int tahun, int jmlMhs);
 void deleteFirstUniv(University*& head);
 void deleteLastUniv(University*& head);
+void deleteUniv(University*& head, const string& nama);
+
 void insertFirstFac(Faculty*& head, const string& nama, int jmlDosen, int jmlMhs);
 void insertLastFac(Faculty*& head, const string& nama, int jmlDosen, int jmlMhs);
 void deleteFirstFac(Faculty*& head);
 void deleteLastFac(Faculty*& head);
+void deleteFac(Faculty*& head, const string& nama);
+
 bool addRelation(University* univ, Faculty* fac);
 bool removeRelation(University* univ, Faculty* fac);
-void deleteUniv(University*& head, const string& nama);
-void deleteFac(Faculty*& head, const string& nama);
+
 University* findUniversity(University* head, const string& name);
 Faculty* findFaculty(Faculty* head, const string& name);
+
 void showAllUniversities(University* head);
 void showFacultiesOfUniversity(University* univ);
 void showUniversitiesWithFaculty(Faculty* fac);
 void showFacultyList(Faculty* head);
 void showUniversityWithMostAndLeastFaculties(University* head);
 void showChildOfParent(University* univ, const string& facName);
+
 void deleteChildOfParent(University* univ, const string& facName);
 int countChildOfParent(University* univ);
 void clearScreen();
